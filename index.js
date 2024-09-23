73ee6f3009707ab77b9e23314b0d60aea5507c02
AkoCR7 <151054667+AkoCR7@users.noreply.github.com>
2024-09-22 17:25:53 -0700

    Add files via upload
    Add

---
+++ index.js
@@ -0,0 +1,286 @@
+onload = function(){
+    let search_icon = this.document.getElementById('display')
+    let display_input = this.document.getElementsByTagName('input')[0]
+    // console.log(search_icon)
+
+    search_icon.onclick = function(){
+    display_input.classList.toggle('AS')
+    // display_input.style.opacity ="1";
+}
+
+// this.addEventListener("click" , function(){
+//     display_search.style.width ='300px'; 
+//     display_search.style.display ='block'; 
+//     display_search.style.marginRight ='10px'; 
+//     display_search.style.opacity ='1'; 
+    
+// })
+
+// Line one
+let video_1_1 = this.document.getElementsByClassName('video-1-1')[0]
+let video_1_2 = this.document.getElementsByClassName('video-1-2')[0]
+let video_1_3 = this.document.getElementsByClassName('video-1-3')[0]
+let video_1_4 = this.document.getElementsByClassName('video-1-4')[0]
+let video_1_5 = this.document.getElementsByClassName('video-1-5')[0]
+let video_1_6 = this.document.getElementsByClassName('video-1-6')[0]
+console.log(video_1_1)
+
+video_1_1.addEventListener("mouseenter" , function(){
+    this.play()
+})
+video_1_1.addEventListener("mouseout" , function(){
+    this.pause()
+})
+// 
+video_1_2.addEventListener("mouseenter" , function(){
+    this.play()
+})
+video_1_2.addEventListener("mouseout" , function(){
+    this.pause()
+})
+// 
+video_1_3.addEventListener("mouseenter" , function(){
+    this.play()
+})
+video_1_3.addEventListener("mouseout" , function(){
+    this.pause()
+})
+// 
+video_1_4.addEventListener("mouseenter" , function(){
+    this.play()
+})
+video_1_4.addEventListener("mouseout" , function(){
+    this.pause()
+})
+// 
+video_1_5.addEventListener("mouseenter" , function(){
+    this.play()
+})
+video_1_5.addEventListener("mouseout" , function(){
+    this.pause()
+})
+// 
+video_1_6.addEventListener("mouseenter" , function(){
+    this.play()
+})
+video_1_6.addEventListener("mouseout" , function(){
+    this.pause()
+})
+
+// Line two
+let video_2_1 = this.document.getElementsByClassName('video-2-1')[0]
+let video_2_2 = this.document.getElementsByClassName('video-2-2')[0]
+let video_2_3 = this.document.getElementsByClassName('video-2-3')[0]
+let video_2_4 = this.document.getElementsByClassName('video-2-4')[0]
+let video_2_5 = this.document.getElementsByClassName('video-2-5')[0]
+let video_2_6 = this.document.getElementsByClassName('video-2-6')[0]
+
+
+video_2_1.addEventListener("mouseenter" , function(){
+    this.play()
+})
+video_2_1.addEventListener("mouseout" , function(){
+    this.pause()
+})
+// 
+video_2_2.addEventListener("mouseenter" , function(){
+    this.play()
+})
+video_2_2.addEventListener("mouseout" , function(){
+    this.pause()
+})
+// 
+video_2_3.addEventListener("mouseenter" , function(){
+    this.play()
+})
+video_2_3.addEventListener("mouseout" , function(){
+    this.pause()
+})
+// 
+video_2_4.addEventListener("mouseenter" , function(){
+    this.play()
+})
+video_2_4.addEventListener("mouseout" , function(){
+    this.pause()
+})
+// 
+video_2_5.addEventListener("mouseenter" , function(){
+    this.play()
+})
+video_2_5.addEventListener("mouseout" , function(){
+    this.pause()
+})
+// 
+video_2_6.addEventListener("mouseenter" , function(){
+    this.play()
+})
+video_2_6.addEventListener("mouseout" , function(){
+    this.pause()
+})
+
+// Line three
+let video_3_1 = this.document.getElementsByClassName('video-3-1')[0]
+let video_3_2 = this.document.getElementsByClassName('video-3-2')[0]
+let video_3_3 = this.document.getElementsByClassName('video-3-3')[0]
+let video_3_4 = this.document.getElementsByClassName('video-3-4')[0]
+let video_3_5 = this.document.getElementsByClassName('video-3-5')[0]
+let video_3_6 = this.document.getElementsByClassName('video-3-6')[0]
+let video_3_7 = this.document.getElementsByClassName('video-3-7')[0]
+
+video_3_1.addEventListener("mouseenter" , function(){
+    this.play()
+})
+video_3_1.addEventListener("mouseout" , function(){
+    this.pause()
+})
+//
+video_3_2.addEventListener("mouseenter" , function(){
+    this.play()
+})
+video_3_2.addEventListener("mouseout" , function(){
+    this.pause()
+})
+// 
+video_3_3.addEventListener("mouseenter" , function(){
+    this.play()
+})
+video_3_3.addEventListener("mouseout" , function(){
+    this.pause()
+})
+// 
+video_3_4.addEventListener("mouseenter" , function(){
+    this.play()
+})
+video_3_4.addEventListener("mouseout" , function(){
+    this.pause()
+})
+// 
+video_3_5.addEventListener("mouseenter" , function(){
+    this.play()
+})
+video_3_5.addEventListener("mouseout" , function(){
+    this.pause()
+})
+// 
+video_3_6.addEventListener("mouseenter" , function(){
+    this.play()
+})
+video_3_6.addEventListener("mouseout" , function(){
+    this.pause()
+})
+// 
+video_3_7.addEventListener("mouseenter" , function(){
+    this.play()
+})
+video_3_7.addEventListener("mouseout" , function(){
+    this.pause()
+}) 
+
+
+// Line foure
+let video_4_1 = this.document.getElementsByClassName('video-4-1')[0]
+let video_4_2 = this.document.getElementsByClassName('video-4-2')[0]
+let video_4_3 = this.document.getElementsByClassName('video-4-3')[0]
+let video_4_4 = this.document.getElementsByClassName('video-4-4')[0]
+let video_4_5 = this.document.getElementsByClassName('video-4-5')[0]
+let video_4_6 = this.document.getElementsByClassName('video-4-6')[0]
+
+video_4_1.addEventListener("mouseenter" , function(){
+    this.play()
+})
+video_4_1.addEventListener("mouseout" , function(){
+    this.pause()
+}) 
+// 
+video_4_2.addEventListener("mouseenter" , function(){
+    this.play()
+})
+video_4_2.addEventListener("mouseout" , function(){
+    this.pause()
+}) 
+// 
+video_4_3.addEventListener("mouseenter" , function(){
+    this.play()
+})
+video_4_3.addEventListener("mouseout" , function(){
+    this.pause()
+}) 
+// 
+video_4_4.addEventListener("mouseenter" , function(){
+    this.play()
+})
+video_4_4.addEventListener("mouseout" , function(){
+    this.pause()
+}) 
+// 
+video_4_5.addEventListener("mouseenter" , function(){
+    this.play()
+})
+video_4_5.addEventListener("mouseout" , function(){
+    this.pause()
+}) 
+// 
+video_4_6.addEventListener("mouseenter" , function(){
+    this.play()
+})
+video_4_6.addEventListener("mouseout" , function(){
+    this.pause()
+}) 
+// 
+video_4_7.addEventListener("mouseenter" , function(){
+    this.play()
+})
+video_4_7.addEventListener("mouseout" , function(){
+    this.pause()
+})  
+// Line five
+let video_5_1 = this.document.getElementsByClassName('video-5-1')[0]
+let video_5_2 = this.document.getElementsByClassName('video-5-2')[0]
+let video_5_3 = this.document.getElementsByClassName('video-5-3')[0]
+let video_5_4 = this.document.getElementsByClassName('video-5-4')[0]
+let video_5_5 = this.document.getElementsByClassName('video-5-5')[0]
+let video_5_6 = this.document.getElementsByClassName('video-5-6')[0]
+
+video_5_1.addEventListener("mouseenter" , function(){
+    this.play()
+})
+video_5_1.addEventListener("mouseout" , function(){
+    this.pause()
+}) 
+// 
+video_5_2.addEventListener("mouseenter" , function(){
+    this.play()
+})
+video_5_2.addEventListener("mouseout" , function(){
+    this.pause()
+}) 
+// 
+video_5_3.addEventListener("mouseenter" , function(){
+    this.play()
+})
+video_5_3.addEventListener("mouseout" , function(){
+    this.pause()
+}) 
+// 
+video_5_4.addEventListener("mouseenter" , function(){
+    this.play()
+})
+video_5_4.addEventListener("mouseout" , function(){
+    this.pause()
+}) 
+// 
+video_5_5.addEventListener("mouseenter" , function(){
+    this.play()
+})
+video_5_5.addEventListener("mouseout" , function(){
+    this.pause()
+})
+// 
+video_5_6.addEventListener("mouseenter" , function(){
+    this.play()
+})
+video_5_6.addEventListener("mouseout" , function(){
+    this.pause()
+})
+// 
+}
